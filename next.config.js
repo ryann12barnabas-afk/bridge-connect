a7b10cafcc01c26285a0aeff25d3229a64621103
SmartRyan <ryann12barnabas@gmail.com>
2026-07-25 11:09:53 +0300

    Add Cloudinary to remote patterns in config

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
       { protocol: 'https', hostname: 'firebasestorage.googleapis.com' },
       { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
       { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'res.cloudinary.com' },
     ],
   },
   async headers() {
